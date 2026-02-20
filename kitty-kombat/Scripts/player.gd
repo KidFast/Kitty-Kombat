787767a69041a2b088b57a55efb594d4ca6bf45f
Potato50006 <sofia.yatsun@gmail.com>
2026-02-20 13:04:31 -0600

    basic player movement

---
+++ kitty-kombat/Scripts/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed = 400
+@export var player_number : int
+
+func get_input():
+	var input_direction
+	if(player_number == 1):
+		input_direction = Input.get_vector("p1_left", "p1_right", "p1_up", "p1_down")
+	elif(player_number == 2):
+		input_direction = Input.get_vector("p2_left", "p2_right", "p2_up", "p2_down")
+	
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+		get_input()
+		move_and_slide()
